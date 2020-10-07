a51a65162faf7be97d143ece1f0a838a6a85fcff
snk986 <231827734@qq.com>
2020-10-07 22:12:43 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+<img alt="Vue logo" src="./assets/logo.png" />
+<div>hi</div>
+<Frank msg="Hello Vue 3.0 + Vite" />
+</template>
+
+<script>
+import Frank from './components/Frank.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Frank
+  }
+}
+</script>
---
+++ src/components/Frank.vue
@@ -0,0 +1,5 @@
+<template>
+<div>
+  My First component
+</div>
+</template>
