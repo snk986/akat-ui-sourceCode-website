afd67699406484ece13c2e2bfda9104ca09582ed
snk986 <231827734@qq.com>
2021-02-17 02:48:39 +0800

    基本完成官网框架

---
+++ src/components/DocDemo.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    默认 的文档
+    <p>默认 的文档</p>
+  </div>
+</template>
--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,11 @@
 import { createWebHashHistory, createRouter } from "vue-router";
 import  Home  from './views/Home.vue';
 import  Doc  from './views/Doc.vue';
+import  DocDemo  from './components/DocDemo.vue';
 import  Switch  from './components/Switch.vue';
+import  Button  from './components/Button.vue';
+import  Dialog  from './components/Dialog.vue';
+import  Tabs  from './components/Tabs.vue';
 
 const history = createWebHashHistory()
 export const router = createRouter({
@@ -10,7 +14,11 @@ export const router = createRouter({
     {path: '/', component: Home},
     {path: '/doc', component: Doc,
       children: [
-        {path: 'switch', component: Switch}
+        {path: '', component: DocDemo},
+        {path: 'switch', component: Switch},
+        {path: 'button', component: Button},
+        {path: 'dialog', component: Dialog},
+        {path: 'tabs', component: Tabs},
       ]
     },
   ]
