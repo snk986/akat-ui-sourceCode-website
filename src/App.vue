bbb4b9f65e4c1e70c5465722aa7f10dad655b297
snk986 <231827734@qq.com>
2021-02-17 02:05:03 +0800

    路由切换之后关闭 aside

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,18 @@
 
 <script>
 import { ref, provide } from 'vue'
+import { router } from './router'
 export default {
   name: 'App',
   setup(props) {
     const width = document.documentElement.clientWidth
     const asideVisible = ref(width <= 500 ? false : true)
     provide('aside', asideVisible)  //set
+    router.afterEach(() => {
+      if(width <= 500) {
+        asideVisible.value = false
+      }
+    })
   }
 }
 </script>
---
+++ src/components/Switch.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    Switch page
+  </div>
+</template>
