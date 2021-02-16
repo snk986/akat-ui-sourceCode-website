54f55c83a7b044fea6cce6810f5fd691e951b674
snk986 <231827734@qq.com>
2021-02-16 22:57:49 +0800

    初始化vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <template>
-<img alt="Vue logo" src="./assets/logo.png" />
-<div>hi</div>
-<Frank msg="Hello Vue 3.0 + Vite" />
+<div>导航栏 |
+  <router-link to="/">Frank</router-link> |
+  <router-link to="/frank2">Frank2</router-link>
+</div>
+<router-view></router-view>
 </template>
 
 <script>
-import Frank from './components/Frank.vue'
-
 export default {
-  name: 'App',
-  components: {
-    Frank
-  }
+  name: 'App'
 }
 </script>
