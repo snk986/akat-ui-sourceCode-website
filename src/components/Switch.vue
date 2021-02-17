4c248bafd47babcc67df8ab718756b791e90f62d
snk986 <231827734@qq.com>
2021-02-17 18:42:21 +0800

    子组件接收value并触发input事件

--- src/components/Switch.vue
+++ src/components/Switch.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
-    <SwitchLib />
+    <SwitchLib :value="checked" @input="checked=$event"/>
   </div>
 </template>
 <script lang="ts">
   import SwitchLib from '../lib/SwitchLib.vue'
+import { ref } from 'vue'
   export default {
-    components: {SwitchLib}  
+    components: {SwitchLib},
+    setup() {
+      const checked = ref(false)
+      return {checked}
+    }  
   }
 </script> 
