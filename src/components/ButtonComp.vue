fa4a04c49736a50a428d7664ffd4d1739a551b38
snk986 <231827734@qq.com>
2021-02-17 22:31:55 +0800

    button中vue3的属性绑定

--- src/components/ButtonComp.vue
+++ src/components/ButtonComp.vue
@@ -4,12 +4,22 @@
   </div>
   <h1>示例1</h1>
   <div>
-    <Button>nihao </Button>
+    <Button 
+      @click="onClick"
+      @focus="onClick"
+      @mouseover="onClick"
+    >nihao </Button>
   </div>
 </template>
 <script lang="ts">
 import Button from '../lib/Button.vue'
 export default {
-  components: {Button}
+  components: {Button},
+  setup(props) {
+    const onClick = () => {
+      console.log('hi');
+    }
+    return {onClick}
+  }
 }
 </script>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,10 +1,18 @@
 <template>
-  <button>
-    <slot />
-  </button>
+  <div :size="size">
+    <button v-bind="rest">
+      <slot />
+    </button>
+  </div>
 </template>
 <script lang="ts">
 export default {
-  
+  // vu3属性绑定：1、默认所有属性都绑定到根元素；2、使用inheritAttrs: false,取消默认绑定；3、使用$attrs或者context.attrs获取所有属性
+  // 4、使用v-bind="$attrs"批量绑定属性 5、使用const {size, ...rest} = context.attrs将属性分开
+  inheritAttrs: false,
+  setup(props,context) {
+    const {size, ...rest} = context.attrs
+    return {size, rest}
+  }
 }
 </script>
