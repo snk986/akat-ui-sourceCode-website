92ed260ef1976fe30dbdf74c1846f59e2e9ef144
snk986 <231827734@qq.com>
2021-02-17 20:13:24 +0800

    优化CSS代码

--- src/lib/SwitchLib.vue
+++ src/lib/SwitchLib.vue
@@ -1,59 +1,66 @@
 <template>
   <div>
-    <button @click="toggle" :class="{checked:value}"> <span></span> </button>
+    <button @click="toggle" :class="{checked:value}">
+      <span></span>
+    </button>
   </div>
 </template>
 <script lang="ts">
-import { ref } from 'vue'
+import { ref } from "vue";
 export default {
-  props:{
-    value: Boolean 
+  props: {
+    value: Boolean
   },
-  setup(props, context){
-    console.log(props.value,111);
-    
-    const toggle = ()=>{
-      context.emit('update:value', !props.value)
+  setup(props, context) {
+    console.log(props.value, 111);
+
+    const toggle = () => {
+      context.emit("update:value", !props.value);
       // this.$emit()
-    }
-    return {toggle}
-  } 
-}
+    };
+    return { toggle };
+  }
+};
 </script>
 <style lang="scss" scoped>
-  $h: 22px;
-  $h2: $h - 4px;
-  button{
-    height: $h;
-    width: $h*2;
-    border: none;
-    background: #dcdfe6;
-    border-radius: $h/2;
-    position: relative;
-  }
-  span{
+$h: 22px;
+$h2: $h - 4px;
+button {
+  height: $h;
+  width: $h * 2;
+  border: none;
+  background: #dcdfe6;
+  border-radius: $h/2;
+  position: relative;
+  > span {
     position: absolute;
     top: 2px;
     left: 2px;
     height: $h2;
     width: $h2;
-    background:white;
+    background: white;
     border-radius: $h2 / 2;
     transition: all 300ms;
   }
-  button.checked{
+  &.checked {
     background: #409eff;
+    > span {
+      left: calc(100% - #{$h2} - 2px);
+    }
   }
-   button.checked > span {
-    left: calc(100% - #{$h2} - 2px);
-  }
-  button:focus {
+  &:focus {
     outline: none;
   }
-  button:active{
-    > span {width: $h2 + 4px;}
+  &:active {
+    > span {
+      width: $h2 + 4px;
+    }
   }
-  button.checked:active{
-    > span {width: $h2 + 4px; margin-left: -4px;}
+  &.checked:active {
+    > span {
+      width: $h2 + 4px;
+      margin-left: -4px;
+    }
   }
+}
 </style>
