d3d94e147357125a5b7a1858c841284e921e60ef
snk986 <231827734@qq.com>
2021-03-03 01:34:26 +0800

    size属性css2

--- src/components/ButtonComp.vue
+++ src/components/ButtonComp.vue
@@ -5,16 +5,26 @@
   <h1>示例1</h1>
   <div class="button-box">
     <Button>按钮</Button>
-    <Button theme="button">按钮</Button>
-    <Button theme="link">按钮</Button>
-    <Button theme="text">按钮</Button>
+    <Button theme="button">普通按钮</Button>
+    <Button theme="link">链接按钮</Button>
+    <Button theme="text">文本按钮</Button>
   </div>
   <h1>示例2</h1>
   <div class="button-box">
     <Button size="big">大按钮</Button>
-    <Button>普通</Button>
+    <Button>普通按钮</Button>
     <Button size="small">小按钮</Button>
   </div>
+  <div>
+      <Button theme="link" size="big">大按钮</Button>
+      <Button theme="link">普通按钮</Button>
+      <Button size="small" theme="link">小按钮</Button>
+    </div>
+    <div>
+      <Button size="big" theme="text">大按钮</Button>
+      <Button theme="text">普通按钮</Button>
+      <Button size="small" theme="text">小按钮</Button>
+    </div>
 </template>
 <script lang="ts">
 import Button from '../lib/Button.vue'
