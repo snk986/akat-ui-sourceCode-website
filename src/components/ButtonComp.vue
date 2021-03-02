1556a042fd84fdfc07a5b64f2375a27606002170
snk986 <231827734@qq.com>
2021-03-03 01:28:35 +0800

    支持size属性

--- src/components/ButtonComp.vue
+++ src/components/ButtonComp.vue
@@ -9,6 +9,12 @@
     <Button theme="link">按钮</Button>
     <Button theme="text">按钮</Button>
   </div>
+  <h1>示例2</h1>
+  <div class="button-box">
+    <Button size="big">大按钮</Button>
+    <Button>普通</Button>
+    <Button size="small">小按钮</Button>
+  </div>
 </template>
 <script lang="ts">
 import Button from '../lib/Button.vue'
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,12 +1,12 @@
 <template>
-    <button v-bind="rest" class="gulu-button"
-      :class="`gulu-theme-${theme}`"
+    <button class="gulu-button"
+      :class="classes"
     >
       <slot />
     </button>
-  </div>
 </template>
 <script lang="ts">
+import { computed } from 'vue'
 export default {
   // vu3属性绑定：1、默认所有属性都绑定到根元素；2、使用inheritAttrs: false,取消默认绑定；3、使用$attrs或者context.attrs获取所有属性
   // 4、使用v-bind="$attrs"批量绑定属性 5、使用const {size, ...rest} = context.attrs将属性分开
@@ -19,10 +19,21 @@ export default {
       type: String,
       default: 'button',
     },
+    size: {
+      type: String,
+      default: 'normal',
+    },
   },
-  setup(props,context) {
-    const {size, ...rest} = context.attrs
-    return {size, rest}
+  setup(props) {
+    const {theme, size} = props
+    const classes = computed(() => {
+      // 这句话作用：对象里[]是key，:后面是value，如果theme是未定义的，key就失效，那么返回空对象，反之，返回key
+      return {
+        [`gulu-theme-${theme}`]: theme,
+        [`gulu-size-${size}`]: size
+      }
+    })
+    return { classes }
   }
 }
 </script>
@@ -31,15 +42,15 @@ export default {
 // UI库的css注意事项
 // 1、不能使用scoped，因为data-v-xxx每次运行可能不同，必须输出稳定不变的class选择器，方便使用者覆盖
 // 2、必须加前缀，.button、.theme-link不行，容易被使用者覆盖；.gulu-button、.gulu-theme-link可以，不容易覆盖
-$h: 32px;
+$h: 16px;
 $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
 .gulu-button {
   box-sizing: border-box;
-  // width: $h*2;
-  height: $h;
+  // width: $h*4;
+  height: $h*2;
   padding: 0 12px;
   cursor: pointer;
   white-space: nowrap; //	文本不会换行，文本会在在同一行上继续，直到遇到 <br> 标签为止。
@@ -77,6 +88,18 @@ $radius: 4px;
     color: inherit;
     &:hover,&:focus{
       background: darken(white, 5%);;
+    }
+  }
+  &.gulu-theme-button{
+    &.gulu-size-big{
+      height: $h*3;
+      font-size: 24px;
+      padding: 0 16px
+    }
+    &.gulu-size-small{
+      font-size: 12px;
+      height: 20px;
+      padding: 0 4px;
     }
   }
 }
