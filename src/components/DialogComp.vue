147b17a274ea0e7827705a416348db3be3f05ddd
snk986 <231827734@qq.com>
2021-03-04 03:47:28 +0800

    Dialog支持关闭

--- src/components/DialogComp.vue
+++ src/components/DialogComp.vue
@@ -2,7 +2,16 @@
   <div>Dialog 示例</div>
   <h1>示例1</h1>
   <Button @click="toggle">toggle</Button>
-  <Dialog :visible="x"></Dialog>
+  <!-- 两种写法 -->
+  <!-- <Dialog :visible="x" @update:visible="x = $event"></Dialog> -->
+  <!-- <Dialog v-model:visible="x"></Dialog> -->
+  <!-- 点击遮罩层不能关闭 -->
+  <Dialog 
+  v-model:visible="x" 
+  :closeOnClickOverlay="false" 
+  :confirm="fn1"
+  :cancel="fn2"
+  ></Dialog>
 </template>
 <script lang="ts">
 import Dialog from "../lib/Dialog.vue";
@@ -18,7 +27,14 @@ export default {
     const toggle = () => {
       x.value = !x.value
     }
-    return { x , toggle }
+    const fn1 = () => {
+      console.log("confirm");
+      return false
+    }
+    const fn2 = () => {
+      console.log("cancel");
+    }
+    return { x , toggle, fn1, fn2 }
   }
 };
 </script>
--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,16 +1,19 @@
 <template>
   <template v-if="visible">
-    <div class="gulu-dialog-overlay"></div>
+    <div class="gulu-dialog-overlay" @click="clickOnOverlay"></div>
     <div class="gulu-dialog-wraper">
       <div class="gulu-dialog" >
-        <header>标题</header>
+        <header>
+          标题
+          <span @click="close" class="gulu-dialog-close"> X </span>
+        </header>
         <main>
           <p>第一行字</p>
           <p>第二行字</p>
         </main>
         <footer>
-          <Button level="main">确定</Button>
-          <Button>取消</Button>
+          <Button level="main" @click="confirm">确定</Button>
+          <Button @click="cancel">取消</Button>
         </footer>
       </div>
     </div>
@@ -23,9 +26,41 @@ export default {
     visible: {
       type: Boolean,
       default: false
+    },
+    closeOnClickOverlay: {
+      type: Boolean,
+      default: true
+    },
+    confirm: {
+      type: Function
+    },
+    cancel: {
+      type: Function
     }
   },
-  components: { Button }
+  components: { Button },
+  setup(props, context) {
+    const close = () => {
+      context.emit('update:visible', false)
+    }
+    const clickOnOverlay = () => {
+      if(props.closeOnClickOverlay) {
+        close()
+      }
+    }
+    const confirm = () => {
+      // js最新语法：可选链 props.confirm?.() !== false
+      if(props.confirm && props.confirm() !== false) {
+        close()
+      }
+    }
+    const cancel = () => {
+      if(props.cancel && props.cancel() !== false) {
+        close()
+      }
+    }
+    return { close, clickOnOverlay, confirm, cancel }
+  }
 };
 </script>
 <style lang="scss" scoped>
