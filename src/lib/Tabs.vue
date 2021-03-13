010d472d5b38623a194d7c12c0f8b10a3180bbec
snk986 <231827734@qq.com>
2021-03-13 19:12:17 +0800

    实现下划线滑动

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -4,13 +4,15 @@
     <!-- <component :is="defaults[0]" />
     <component :is="defaults[1]" /> -->
     <div class="gulu-tabs">
-      <div class="gulu-tabs-nav">
+      <div class="gulu-tabs-nav" ref="container">
         <div class="gulu-tabs-nav-item"
           :class="{selected: t === selected}"
-          v-for="(t,index) in titles" :key="index"
+          v-for="(t,index) in titles"
+          :ref="el => { if (el) navItems[index] = el}" 
+          :key="index"
           @click="select(t)"
         >{{t}}</div>
-        <div class="gulu-tabs-nav-indicator"></div>
+        <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
       </div>
     <div class="gulu-tabs-content">
       <!-- v-for与v-if官方不推荐一起使用 -->
@@ -26,7 +28,7 @@
 </template>
 <script lang="ts">
 import Tab from "./Tab.vue"
-import { computed } from 'vue';
+import { computed, ref, onMounted, onUpdated } from 'vue';
 export default {
   props: {
     selected: {
@@ -40,7 +42,7 @@ export default {
     // 第二步
     const defaults = context.slots.default()
     // 每一个Tab.vue组件最终会导出为一个对象
-    console.log(defaults[0].type === Tab);
+    // console.log(defaults[0].type === Tab);
     defaults.map((tag) => {
       if(tag.type !== Tab){
         throw new Error('Tabs组件的子标签必须是Tab')
@@ -49,7 +51,7 @@ export default {
     // 当前选中组件：setup只会在页面挂载的时候初始化一遍，意味着所有函数都只执行一遍，所有属性都只计算一遍，
     // 所以要引入计算函数computed，每次点击的时候对current重新计算
     const current = computed(() => {
-      console.log('重新return');
+      // console.log('重新return');
       return defaults.filter((tag) => {
         return tag.props.title === props.selected
       })[0]
@@ -63,8 +65,31 @@ export default {
       // console.log(title,555);
       context.emit('update:selected', title)
     }
+    const navItems = ref<HTMLDivElement[]>([])
+    const indicator = ref<HTMLDivElement>(null)
+    const container = ref<HTMLDivElement>(null)
+    const navRender = () => {
+      // onMounted只在第一次渲染执行,所以代码也要放onUpdated里面，
+      // 为何onMounted也要放一次这个逻辑的代码：不放的话第一次加载导航蓝条会直接在第一个导航条，显示不正确，也无法满足导航条放在第二个的需求
+      // console.log({...navItems.value});
+      const divs = navItems.value
+      // classList没有代码提示，应为div的类型为any，说明ts并不知道div是一个元素,加了ts泛型写法<HTMLDivElement[]>后，就会有提示
+      const result = divs.filter(div => div.classList.contains('selected'))[0]
+      // const result = divs.find(div => div.classList.contains('selected')) find写法低版本，浏览器不支持
+      // console.log(result);
+      const { width } = result.getBoundingClientRect()
+      indicator.value.style.width = width + 'px'   
+      const { left:left1 } = container.value.getBoundingClientRect()
+      const { left:left2 } = result.getBoundingClientRect()
+      const left = left2 - left1
+      indicator.value.style.left = left + 'px'
+    }
+    // @ts-ignore
+    onMounted(navRender)
+    // @ts-ignore
+    onUpdated(navRender)
     return {
-      defaults, titles, current, select
+      defaults, titles, current, select, navItems, indicator, container
     }
   }
 }
@@ -97,6 +122,7 @@ $border-color: #d9d9d9;
       left: 0;
       bottom: -1px;
       width: 100px;
+      transition: all 250ms;
     }
   }
   &-content {
