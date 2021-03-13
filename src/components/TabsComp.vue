55a658b53b07fb713f314938b72f48132bdf6007
snk986 <231827734@qq.com>
2021-03-13 14:26:27 +0800

    遇到Vue3bug：github.com/vuejs/vue-next/issues/2013

--- src/components/TabsComp.vue
+++ src/components/TabsComp.vue
@@ -1,24 +1,26 @@
 <template>
-  <div>
-    Tabs 的文档
-  </div>
+  <div>Tabs 的文档</div>
   <h1>示例1</h1>
-  <Tabs>
-    <Tab title="导航1">
-      内容1
-    </Tab>
-    <Tab title="导航2">
-      内容2
-    </Tab>
+  <Tabs v-model:selected="x">
+    <Tab title="导航1">内容1</Tab>
+    <Tab title="导航2">内容2</Tab>
   </Tabs>
+  <!-- <p>{{x}}</p> -->
 </template>
 <script lang="ts">
-import Tabs from "../lib/Tabs.vue"
-import Tab from "../lib/Tab.vue"
+import Tabs from "../lib/Tabs.vue";
+import Tab from "../lib/Tab.vue";
+import { ref } from 'vue';
 export default {
   components: {
     Tabs,
     Tab
+  },
+  setup(props) {
+    const x = ref('导航1')
+    return {
+      x
+    }
   }
-}
+};
 </script>
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,16 +1,33 @@
 <template>
-  <div>
     <!-- {{defaults[0]}} ++++  第二步
     {{defaults[1]}} -->
     <!-- <component :is="defaults[0]" />
     <component :is="defaults[1]" /> -->
-    <div v-for="(t,index) in titles" :key="index">{{t}}</div>
-    <component v-for="(c,index) in defaults" :is="c" :key="index"/>
+    <div class="gulu-tabs">
+      <div class="gulu-tabs-nav">
+        <div class="gulu-tabs-nav-item"
+          :class="{selected: t === selected}"
+          v-for="(t,index) in titles" :key="index"
+          @click="select(t)"
+        >{{t}}</div>
+      </div>
+    <div class="gulu-tabs-content">
+      <!-- v-for与v-if官方不推荐一起使用 -->
+      <!-- <component class="gulu-tabs-content-item" v-for="(c,index) in defaults" v-if="c.props.title === selected" :is="c" :key="index" /> -->
+      <component class="gulu-tabs-content-item" :is="current" />
+      {{current}}
+    </div>
   </div>
 </template>
 <script lang="ts">
 import Tab from "./Tab.vue"
+import { computed } from 'vue';
 export default {
+  props: {
+    selected: {
+      type: String
+    }
+  },
   setup(props, context) {
     // 第一步
     // console.log({...context.slots.default()[0]});
@@ -24,13 +41,52 @@ export default {
         throw new Error('Tabs组件的子标签必须是Tab')
       }
     })
+    // 当前选中组件：setup只会在页面挂载的时候初始化一遍，意味着所有函数都只执行一遍，所有属性都只计算一遍，
+    // 所以要引入计算函数computed，每次点击的时候对current重新计算
+    const current = computed(() => {
+      console.log('重新return');
+      return defaults.filter((tag) => {
+        return tag.props.title === props.selected
+      })[0]
+    }) 
+    // 所有标题
     const titles = defaults.map((tag) => {
       return tag.props.title
     })
-    console.log(titles); 
+    // 点击导航切换
+    const select = (title: string) => {
+      // console.log(title,555);
+      context.emit('update:selected', title)
+    }
     return {
-      defaults, titles
+      defaults, titles, current, select
     }
   }
 }
 </script>
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.gulu-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+  }
+}
+</style>
