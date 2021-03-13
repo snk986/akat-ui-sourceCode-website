22ea2cfb6075c5dbfa8476ca835beda94312fd0f
snk986 <231827734@qq.com>
2021-03-13 17:54:25 +0800

    indicator css 1

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -10,6 +10,7 @@
           v-for="(t,index) in titles" :key="index"
           @click="select(t)"
         >{{t}}</div>
+        <div class="gulu-tabs-nav-indicator"></div>
       </div>
     <div class="gulu-tabs-content">
       <!-- v-for与v-if官方不推荐一起使用 -->
@@ -77,6 +78,7 @@ $border-color: #d9d9d9;
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+    position: relative;
     &-item {
       padding: 8px 0;
       margin: 0 16px;
@@ -88,6 +90,14 @@ $border-color: #d9d9d9;
         color: $blue;
       }
     }
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+    }
   }
   &-content {
     padding: 8px 0;
@@ -98,5 +108,6 @@ $border-color: #d9d9d9;
       }
     }
   }
+
 }
 </style>
