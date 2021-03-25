82a4446d713452929722b7418abd2da24d70bd6b
snk986 <231827734@qq.com>
2021-03-25 22:43:09 +0800

    优化官网

--- src/components/DocDemo.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    默认 的文档
-    <p>默认 的文档</p>
-  </div>
-</template>
--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,6 @@
 import { createWebHashHistory, createRouter } from "vue-router";
 import  Home  from './views/Home.vue';
 import  Doc  from './views/Doc.vue';
-import  DocDemo  from './components/DocDemo.vue';
 import  SwitchComp  from './components/SwitchComp.vue';
 import  ButtonComp  from './components/ButtonComp.vue';
 import  DialogComp  from './components/DialogComp.vue';
@@ -17,7 +16,7 @@ export const router = createRouter({
     {path: '/', component: Home},
     {path: '/doc', component: Doc,
       children: [
-        {path: '', component: DocDemo},
+        {path: '', redirect: '/doc/intro'},
         {path: 'intro', component: md('intro')},
         {path: 'install', component: md('install')},
         {path: 'get-started', component: md('getStarted')},
