e835d6ed9b8dda8961e38a94e90640c0ce1ff84c
snk986 <231827734@qq.com>
2021-03-26 03:14:00 +0800

    改造 Button Demo

--- src/components/ButtonComp.vue
+++ src/components/ButtonComp.vue
@@ -1,79 +1,30 @@
 <template>
   <div>
-    Button 示例
-  </div>
-  <h1>示例1</h1>
-  <div class="button-box">
-    <Button>按钮</Button>
-    <Button theme="button">普通按钮</Button>
-    <Button theme="link">链接按钮</Button>
-    <Button theme="text">文本按钮</Button>
-  </div>
-  <h1>示例2</h1>
-  <div class="button-box">
-    <Button size="big">大按钮</Button>
-    <Button>普通按钮</Button>
-    <Button size="small">小按钮</Button>
-  </div>
-  <div class="button-box">
-      <Button theme="link" size="big">大按钮</Button>
-      <Button theme="link">普通按钮</Button>
-      <Button size="small" theme="link">小按钮</Button>
-    </div>
-    <div class="button-box">
-      <Button size="big" theme="text">大按钮</Button>
-      <Button theme="text">普通按钮</Button>
-      <Button size="small" theme="text">小按钮</Button>
-    </div>
-    <h1>示例3</h1>
-  <div class="button-box">
-     <Button level="main">主要按钮</Button>
-      <Button>普通按钮</Button>
-      <Button level="danger">危险按钮</Button>
-    </div>
-    <div class="button-box">
-      <Button theme="link" level="main">主要链接按钮</Button>
-      <Button theme="link">普通链接按钮</Button>
-      <Button theme="link" level="danger">危险链接按钮</Button>
-    </div>
-    <div class="button-box">
-      <Button theme="text" level="main">主要文字按钮</Button>
-      <Button theme="text">普通文字按钮</Button>
-      <Button theme="text" level="danger">危险文字按钮</Button>
-    </div>
-    <h1>示例4</h1>
-  <div class="button-box">
-    <Button disabled>禁用按钮</Button>
-    <Button theme="link" disabled>禁用链接按钮</Button>
-    <Button theme="text" disabled>禁用文本按钮</Button>
-  </div>
-  <h1>示例5</h1>
-  <div class="button-box">
-    <Button loading>加载中</Button>
-    <Button >加载完毕</Button>
-  </div>
-  <h1>示例5</h1>
-  <div class="button-box">
-    <Button @click="onClick">点击按钮</Button>
+    <div>Button 示例</div>
+    <Demo :component="Button1Demo" />
+    <Demo :component="Button2Demo" />
+    <Demo :component="Button3Demo" />
+    <Demo :component="Button4Demo" />
+    <Demo :component="Button5Demo" />
   </div>
 </template>
 <script lang="ts">
-import Button from '../lib/Button.vue';
+import Demo from "./Demo.vue";
+import Button1Demo from "./Button1.demo.vue";
+import Button2Demo from "./Button2.demo.vue";
+import Button3Demo from "./Button3.demo.vue";
+import Button4Demo from "./Button4.demo.vue";
+import Button5Demo from "./Button5.demo.vue";
 export default {
-  components: { Button },
+  components: { Demo },
   setup(props) {
-    const onClick = () => {
-      console.log('hi');
+    return {
+      Button1Demo,
+      Button2Demo,
+      Button3Demo,
+      Button4Demo,
+      Button5Demo
     }
-    return {onClick}
   }
 }
 </script>
-<style lang="scss" scoped>
-.button-box {
-  // display: inline-flex;  //inline-flex是包裹在一起的元素为一小行(指一行的一小部分)。flex是包裹在一起的元素占一行。inline-block是各个元素占一小行
-  display: flex;
-  // justify-content: center;
-  align-items: center;
-}
-</style>
