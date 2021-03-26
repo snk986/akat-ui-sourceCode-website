f31fafdcd92a94b959fd905126b2e7d13b6a22c4
snk986 <231827734@qq.com>
2021-03-26 16:52:57 +0800

    手机端源码展示宽度溢出出现横向滚动条bug修复

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -56,6 +56,8 @@ $border-color: #d9d9d9;
   &-code {
     padding: 8px 16px;
     border-top: 1px dashed $border-color;
+    // 手机端源码展示宽度溢出出现横向滚动条bug修复
+    width: calc(100vw - 34px);
     >pre {
       line-height: 1.1;
       font-family: Consolas, 'Courier New', Courier, monospace;
